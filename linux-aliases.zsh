3ce40072d090a586d80958c3a84f909aa90efb1d
tymiurge <31214622+tymiurge@users.noreply.github.com>
2025-08-11 13:28:26 +0300

    Update linux-aliases.zsh

--- linux-aliases.zsh
+++ linux-aliases.zsh
@@ -1,3 +1,12 @@
 alias hm="cd ~/"
 alias cl="clear"
 alias gals="cat ~/.oh-my-zsh/custom/git-aliases.zsh"
+function cds() {
+  cd $1
+  ls
+}
+alias lsa="ls -all"
+function mdcd() {
+  mkdir $1
+  cd $1
+}
